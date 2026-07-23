9fc1dff9742f2d9853a156385f42d19a8ca7a28f
an0jin <anyoungjin0106@gmail.com>
2026-07-23 22:25:12 +0900

    Refactor model to store class names and improve API
    - Changed Model to accept class names list instead of just count, storing both
    - Converted classifier and num_classes to @property decorators for cleaner API
    - Added classes property to access stored class names
    - Updated training to pass class list and optimize only classifier with lower LR (1e-4)
    - Added save_classes function to export class names to file

--- DL/personal_color/model.py
+++ DL/personal_color/model.py
@@ -2,19 +2,25 @@ import torch.nn as nn
 import timm
 
 class Model(nn.Module):
-    def __init__(self, num_classes, model='tf_efficientnetv2_s.in21k_ft_in1k'):
+    def __init__(self, classes, model='tf_efficientnetv2_s.in21k_ft_in1k'):
         super(Model, self).__init__()
-        self._num_classes=num_classes
-        self.model = timm.create_model(model, pretrained=True,num_classes=num_classes)
+        self._classes = classes
+        self._num_classes=len(classes)
+        self.model = timm.create_model(model, pretrained=True,num_classes=self.num_classes)
         
         for param in self.model.parameters():
             param.requires_grad = False
         for param in self.model.get_classifier().parameters():
             param.requires_grad = True
-        self.head=self.model.get_classifier()
+    @property
+    def classifier(self):
+        return self.model.get_classifier()
     @property
     def num_classes(self):
         return self._num_classes
+    @property
+    def classes(self):
+        return self._classes
     #오버라이딩
     def forward(self, x):
         return self.model(x)
